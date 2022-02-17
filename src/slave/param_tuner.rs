22782eb86a9f509826d631de7f81748469d3b972
coco24 <1281299809@qq.com>
2022-02-17 13:04:27 +0800

    添加文件版权信息

--- src/slave/param_tuner.rs
+++ src/slave/param_tuner.rs
@@ -1,3 +1,21 @@
+/* param_tuner.rs
+ *
+ * Copyright 2021-2022 Bohong Huang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 use std::{fmt::Debug, cmp::{max, min}, collections::{HashMap, VecDeque}, ops::Deref, time::{SystemTime, Duration}, io::Error as IOError};
 use async_std::{net::TcpStream, task, prelude::*};
 
