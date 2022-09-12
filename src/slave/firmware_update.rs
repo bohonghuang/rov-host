2e0cc63fb2dd4b66bd2340cc74e14dea94471ee3
coco24 <1281299809@qq.com>
2022-09-12 14:54:16 +0800

    更改固件更新协议

--- src/slave/firmware_update.rs
+++ src/slave/firmware_update.rs
@@ -44,7 +44,7 @@ pub enum SlaveFirmwareUpdaterMsg {
     NextStep,
     FirmwareFileSelected(PathBuf),
     FirmwareUploadProgressUpdated(f32),
-    FirmwareUploadFailed,
+    FirmwareUploadFailed(SlaveFirmwareUpdateError),
 }
 
 #[tracker::track(pub)]
@@ -56,12 +56,22 @@ pub struct SlaveFirmwareUpdaterModel {
     firmware_uploading_progress: f32,
     #[no_eq]
     _rpc_client: OnceCell<RpcClient>,
+    #[no_eq]
+    #[derivative(Default(value="Ok(())"))]
+    firmware_update_result: Result<(), SlaveFirmwareUpdateError>,
+}
+
+impl SlaveFirmwareUpdaterModel {
+    fn is_uploading(&self) -> bool {
+        self.firmware_uploading_progress > 0.0 && self.firmware_uploading_progress < 1.0 && self.firmware_update_result.is_ok()
+    }
 }
 
 #[derive(Debug)]
 pub enum SlaveFirmwareUpdateError {
     IOError(std::io::Error),
     RpcError(jsonrpsee_core::Error),
+    VerificationError(usize, usize),
 }
 
 impl Display for SlaveFirmwareUpdateError {
@@ -69,6 +79,7 @@ impl Display for SlaveFirmwareUpdateError {
         match self {
             SlaveFirmwareUpdateError::IOError(error) => Display::fmt(error, f),
             SlaveFirmwareUpdateError::RpcError(error) => Display::fmt(error, f),
+            SlaveFirmwareUpdateError::VerificationError(expect, given) => write!(f, "Verification error: The returned length {} doesn't match the expected {}", given, expect),
         }
     }
 }
@@ -106,6 +117,7 @@ impl MicroModel for SlaveFirmwareUpdaterModel {
             },
             SlaveFirmwareUpdaterMsg::StartUpload => {
                 if let Some(path) = self.get_firmware_file_path() {
+                    const CHUNK_SIZE: usize = 1024;
                     send!(sender, SlaveFirmwareUpdaterMsg::NextStep);
                     let rpc_client = self.get_rpc_client().clone();
                     let handle = task::spawn(clone!(@strong sender, @strong path => async move {
@@ -113,24 +125,43 @@ impl MicroModel for SlaveFirmwareUpdaterModel {
                             Ok(mut file) => {
                                 let mut bytes = Vec::new();
                                 file.read_to_end(&mut bytes).await.map_err(SlaveFirmwareUpdateError::IOError)?;
-                                let bytes = bytes.as_slice();
-                                let bytes_encoded = base64::encode(bytes);
-                                rpc_client.request::<()>(METHOD_UPDATE_FIRMWARE, Some(bytes_encoded.to_rpc_params())).await.map(|_| send!(sender, SlaveFirmwareUpdaterMsg::FirmwareUploadProgressUpdated(1.0))).map_err(SlaveFirmwareUpdateError::RpcError)
+                                let len_total = bytes.len();
+                                let mut len_remain = len_total;
+                                for chunk in bytes.chunks(CHUNK_SIZE) {
+                                    let chunk_encoded = base64::encode(chunk);
+                                    match rpc_client.request::<usize>(METHOD_UPDATE_FIRMWARE, Some((chunk_encoded, len_remain).to_rpc_params())).await {
+                                        Ok(len_received) => {
+                                            if len_received == chunk.len() {
+                                                len_remain -= len_received;
+                                                send!(sender, SlaveFirmwareUpdaterMsg::FirmwareUploadProgressUpdated((len_total - len_remain) as f32 / len_total as f32))
+                                            } else {
+                                                return Err(SlaveFirmwareUpdateError::VerificationError(chunk.len(), len_received));
+                                            }
+                                        },
+                                        Err(err) => {
+                                            return Err(SlaveFirmwareUpdateError::RpcError(err));
+                                        },
+                                    }
+                                }
+                                Ok(())
                             },
                             Err(err) => Err(SlaveFirmwareUpdateError::IOError(err)),
                         }
                     }));
                     let handle = task::spawn(async move {
                         let result = handle.await;
-                        if result.is_err() {
-                            send!(sender, SlaveFirmwareUpdaterMsg::FirmwareUploadFailed);
+                        if let Err(err) = result {
+                            send!(sender, SlaveFirmwareUpdaterMsg::FirmwareUploadFailed(err));
                         }
-                        result.map_err(|err| Box::new(err) as Box<dyn Error + Send>)
+                        Ok(())
                     });
                     send!(parent_sender, SlaveMsg::CommunicationMessage(SlaveCommunicationMsg::Block(handle)));
                 }
             },
-            SlaveFirmwareUpdaterMsg::FirmwareUploadFailed => send!(sender, SlaveFirmwareUpdaterMsg::FirmwareUploadProgressUpdated(-1.0)),
+            SlaveFirmwareUpdaterMsg::FirmwareUploadFailed(err) => {
+                self.set_firmware_update_result(Err(err));
+                send!(sender, SlaveFirmwareUpdaterMsg::NextStep);
+            },
         }
     }
 }
@@ -147,7 +178,7 @@ impl MicroWidgets<SlaveFirmwareUpdaterModel> for SlaveFirmwareUpdaterWidgets {
             set_content = Some(&GtkBox) {
                 set_orientation: Orientation::Vertical,
                 append = &HeaderBar {
-                    set_sensitive: track!(model.changed(SlaveFirmwareUpdaterModel::firmware_uploading_progress()), *model.get_firmware_uploading_progress() <= 0.0 || *model.get_firmware_uploading_progress() >= 1.0),
+                    set_sensitive: track!(model.changed(SlaveFirmwareUpdaterModel::firmware_uploading_progress()) || model.changed(SlaveFirmwareUpdaterModel::firmware_update_result()), !model.is_uploading()),
                 },
                 append: carousel = &Carousel {
                     set_hexpand: true,
@@ -188,7 +219,6 @@ impl MicroWidgets<SlaveFirmwareUpdaterModel> for SlaveFirmwareUpdaterWidgets {
                                         connect_clicked(sender, window) => move |_button| {
                                             let filter = FileFilter::new();
                                             filter.add_suffix("tar.gz");
-                                            filter.add_suffix("gz");
                                             filter.set_name(Some("固件文件"));
                                             std::mem::forget(select_path(FileChooserAction::Open, &[filter], &window, clone!(@strong sender => move |path| {
                                                 match path {
@@ -229,11 +259,14 @@ impl MicroWidgets<SlaveFirmwareUpdaterModel> for SlaveFirmwareUpdaterWidgets {
                         },
                     },
                     append = &StatusPage {
-                        set_icon_name: track!(model.changed(SlaveFirmwareUpdaterModel::firmware_uploading_progress()), if *model.get_firmware_uploading_progress() >= 0.0 { Some("emblem-ok-symbolic") } else { Some("dialog-warning-symbolic") }),
-                        set_title: track!(model.changed(SlaveFirmwareUpdaterModel::firmware_uploading_progress()), if *model.get_firmware_uploading_progress() >= 0.0 { "固件更新成功" } else { "固件更新失败" }),
+                        set_icon_name: track!(model.changed(SlaveFirmwareUpdaterModel::firmware_update_result()), if model.get_firmware_update_result().is_ok() { Some("emblem-ok-symbolic") } else { Some("dialog-warning-symbolic") }),
+                        set_title: track!(model.changed(SlaveFirmwareUpdaterModel::firmware_update_result()), if model.get_firmware_update_result().is_ok() { "固件更新成功" } else { "固件更新失败" }),
                         set_hexpand: true,
                         set_vexpand: true,
-                        set_description: track!(model.changed(SlaveFirmwareUpdaterModel::firmware_uploading_progress()), Some(if *model.get_firmware_uploading_progress() >= 0.0 { "机器人将自动重启，请稍后手动进行连接。" } else { "请检查文件与网络连接是否正常。" })),
+                        set_description: track!(model.changed(SlaveFirmwareUpdaterModel::firmware_update_result()), Some(
+                            match model.get_firmware_update_result() {
+                                Ok(_) => "机器人将自动重启，请稍后手动进行连接。".to_string(),
+                                Err(err) => format!("请检查文件与网络连接是否正常。\n\n{}", err)}).as_deref()),
                         set_child = Some(&Button) {
                             set_css_classes: &["suggested-action", "pill"],
                             set_halign: Align::Center,
